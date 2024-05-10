c4589af1ab270c2f123c74e7da40b9c5241ad2fb
Natalie Ayuba <natalieayuba@hotmail.com>
2024-05-10 16:24:25 +0100

    Animated avatar

--- components/nav/Avatar.tsx
+++ components/nav/Avatar.tsx
@@ -1,11 +1,59 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Avatar = () => {
+  const [hovered, setHovered] = useState(false);
+  const [animate, setAnimate] = useState(false);
+  const [src, setSrc] = useState('/robo-nat-eyes-open.svg');
+
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+  useEffect(() => {
+    if (hovered) {
+      const frames = [
+        { src: '/robo-nat-eyes-open.svg', interval: 150 },
+        { src: '/robo-nat-eyes-half-open.svg', interval: 30 },
+        { src: '/robo-nat-eyes-half-closed.svg', interval: 30 },
+        { src: '/robo-nat-eyes-closed.svg', interval: 60 },
+        { src: '/robo-nat-eyes-half-closed.svg', interval: 30 },
+        { src: '/robo-nat-eyes-half-open.svg', interval: 30 },
+        { src: '/robo-nat-eyes-open.svg', interval: 400 },
+        { src: '/robo-nat-eyes-half-open.svg', interval: 20 },
+        { src: '/robo-nat-eyes-half-closed.svg', interval: 20 },
+        { src: '/robo-nat-eyes-closed.svg', interval: 40 },
+        { src: '/robo-nat-eyes-half-closed.svg', interval: 20 },
+        { src: '/robo-nat-eyes-half-open.svg', interval: 20 },
+        { src: '/robo-nat-eyes-open.svg', interval: 150 },
+      ];
+
+      const animate = async () => {
+        setAnimate(true);
+        for (const frame of frames) {
+          setSrc(frame.src);
+          await delay(frame.interval);
+        }
+        setAnimate(false);
+      };
+
+      animate();
+    }
+  }, [hovered]);
+
   return (
-    <Link href='/'>
+    <Link
+      href='/'
+      onMouseEnter={() => {
+        if (!animate) {
+          setHovered(true);
+        }
+      }}
+      onMouseLeave={() => setHovered(false)}
+    >
       <Image
-        src='/robo-nat.svg'
+        src={src}
         alt='Robo-Nat logo'
         width={0}
         height={0}
