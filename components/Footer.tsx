c212fe25a41cbd3d409484eb4dad045d000f4396
Natalie Ayuba <natalieayuba@hotmail.com>
2024-05-13 01:34:55 +0100

    Created footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import Socials from './navbar/Socials';
+
+const Footer = () => {
+  return (
+    <footer className='margin mt-12 py-12 flex flex-col items-center gap-4'>
+      <Socials />
+      <p className='text-sm'>Designed and Developed by Natalie Ayuba</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,13 +1,20 @@
+import { headerHeight } from '@/config';
+import Footer from './Footer';
+
 const PageLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
   return (
-    <div className='bg-gradient-to-b from-blue to-white flex justify-center'>
-      <main className='w-full max-w-[1200px] margin transition-all duration-300'>
+    <div
+      className='bg-gradient-to-b from-blue to-white flex flex-col justify-between'
+      style={{ minHeight: `calc(100vh - ${headerHeight}` }}
+    >
+      <main className='max-w-[1200px] margin transition-all duration-300'>
         {children}
       </main>
+      <Footer />
     </div>
   );
 };
