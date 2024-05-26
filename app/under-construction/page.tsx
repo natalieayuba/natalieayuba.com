2ce3c426e4ac940dff10213b2fa71330b49cda8c
Natalie Ayuba <natalieayuba@hotmail.com>
2024-05-26 17:19:43 +0100

    Updated config

--- app/under-construction/page.tsx
+++ app/under-construction/page.tsx
@@ -10,8 +10,10 @@ const UnderConstruction = () => {
     <PageLayout hideHeader fullHeight>
       <div className='my-auto flex flex-col items-center text-center'>
         <h1 className='font-bold text-6xl mb-6 max-w-sm'>Under Construction</h1>
-        <p>My website is currently undergoing some changes.</p>
-        <p>Please come back soon :)</p>
+        <div className='text-lg'>
+          <p>My website is currently undergoing some changes.</p>
+          <p>Please come back soon :)</p>
+        </div>
       </div>
     </PageLayout>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: "export",
-  basePath: "/nextjs-github-pages",
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   redirects() {
     return [
-      process.env.UNDER_CONSTRUCTION === "1"
-        ? { source: "/((?!under-construction).*)", destination: "/under-construction", permanent: false }
+      process.env.UNDER_CONSTRUCTION === '1'
+        ? {
+            source: '/((?!under-construction).*)',
+            destination: '/under-construction',
+            permanent: false,
+          }
         : null,
     ].filter(Boolean);
-  }
+  },
 };
 
 export default nextConfig;
