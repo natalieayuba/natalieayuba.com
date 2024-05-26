a6ce1dd1fcbda9b2750932fee2d9ad13a21df075
Natalie Ayuba <natalieayuba@hotmail.com>
2024-05-26 22:27:28 +0100

    Updated env config

--- next.config.mjs
+++ next.config.mjs
@@ -5,22 +5,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-};
-
-nextConfig.env = {
-  UNDER_CONSTRUCTION: process.env.UNDER_CONSTRUCTION,
-};
-
-nextConfig.redirects = () => {
-  return [
-    nextConfig.env.UNDER_CONSTRUCTION === '1'
-      ? {
-          source: '/((?!under-construction).*)',
-          destination: '/under-construction',
-          permanent: false,
-        }
-      : null,
-  ].filter(Boolean);
+  redirects() {
+    return [
+      process.env.UNDER_CONSTRUCTION === '1'
+        ? {
+            source: '/((?!under-construction).*)',
+            destination: '/under-construction',
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean);
+  },
 };
 
 export default nextConfig;
