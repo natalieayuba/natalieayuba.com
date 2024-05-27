c1c57f462d09f25059503cc98b59ea051cab0e08
Natalie Ayuba <natalieayuba@hotmail.com>
2024-05-28 00:12:30 +0100

    config test

--- next.config.mjs
+++ next.config.mjs
@@ -6,21 +6,22 @@ const nextConfig = {
     unoptimized: true,
   },
   async redirects() {
-    return process.env.NEXT_PUBLIC_UNDER_CONSTRUCTION === 'true'
-      ? [
-          {
-            source: '/((?!under-construction).*)',
-            destination: '/under-construction',
-            permanent: true,
-          },
-        ]
-      : [
-          {
-            source: '/under-construction',
-            destination: '/',
-            permanent: true,
-          },
-        ];
+    // return process.env.NEXT_PUBLIC_UNDER_CONSTRUCTION === 'true'
+    //   ?
+    [
+      {
+        source: '/((?!under-construction).*)',
+        destination: '/under-construction',
+        permanent: false,
+      },
+    ];
+    // : [
+    //     {
+    //       source: '/under-construction',
+    //       destination: '/',
+    //       permanent: false,
+    //     },
+    //   ];
   },
 };
 
