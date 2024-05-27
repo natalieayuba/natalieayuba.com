7b58b8ebe0e34647b4a6920e05f52905996eddba
Natalie Ayuba <natalieayuba@hotmail.com>
2024-05-28 00:05:45 +0100

    Updated config

--- next.config.mjs
+++ next.config.mjs
@@ -5,20 +5,20 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  redirects() {
+  async redirects() {
     return process.env.NEXT_PUBLIC_UNDER_CONSTRUCTION === 'true'
       ? [
           {
             source: '/((?!under-construction).*)',
             destination: '/under-construction',
-            permanent: false,
+            permanent: true,
           },
         ]
       : [
           {
             source: '/under-construction',
             destination: '/',
-            permanent: false,
+            permanent: true,
           },
         ];
   },
