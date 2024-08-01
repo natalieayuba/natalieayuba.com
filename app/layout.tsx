ff2f3456d47aef5ec8c69b983d51d662792ceb9b
Natalie Ayuba <natalieayuba@hotmail.com>
2024-08-01 06:06:59 +0100

    Renamed var

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,11 @@ export const viewport: Viewport = {
   themeColor: colors.blue,
 };
 
-const isNighttime = new Date().getHours() > 19 || new Date().getHours() < 6;
+const isNight = new Date().getHours() > 19 || new Date().getHours() < 6;
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
-    <html lang='en-GB' className={`scroll-smooth ${isNighttime && ' dark'}`}>
+    <html lang='en-GB' className={`scroll-smooth ${isNight && ' dark'}`}>
       <body
         className={`${poppins.className} leading-relaxed bg-blue dark:bg-navy text-black-alpha-90 dark:text-white-alpha-90 selection:bg-purple-alpha-60 selection:text-black-alpha-90  dark:selection:text-white-alpha-90`}
       >
