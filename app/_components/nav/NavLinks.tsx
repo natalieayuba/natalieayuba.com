0e97b851f1a6cbf950df556e5f54d0481c913a17
Natalie Ayuba <natalieayuba@hotmail.com>
2024-09-16 16:27:02 +0100

    Fixed navlinks animation

--- app/_components/nav/NavLinks.tsx
+++ app/_components/nav/NavLinks.tsx
@@ -2,7 +2,7 @@
 import Socials from './Socials';
 import Link from 'next/link';
 import { useEffect, useState, type MutableRefObject } from 'react';
-import { colors, navLinks, underlines } from '@/config';
+import { colors, navLinks } from '@/config';
 import { interval } from '@/utils/navlinkAnimation';
 export interface NavLinksProps {
   sectionsRef?: MutableRefObject<HTMLElement[]>;
@@ -11,22 +11,45 @@ export interface NavLinksProps {
 
 const NavLinks = ({ sectionsRef, activeLink }: NavLinksProps) => {
   const [active, setActive] = useState(activeLink || '');
+  const [scrollByClick, setScrollByClick] = useState(false);
   const [hovered, setHovered] = useState('');
+  const underlines = {
+    straight:
+      'M6.74023 5.47959C11.8102 5.17959 16.8302 6.01959 21.9002 6.36959C27.7502 6.76959 34.3102 7.02959 39.4602 7.36959C50.7202 8.10959 61.9902 8.89959 73.2602 8.46959',
+    lessStraight:
+      'M6.2998 12.1508C7.0498 10.9108 15.7698 9.65084 17.3998 9.37084C25.2798 8.00084 32.9498 6.89084 40.7598 5.13084C51.3298 2.75084 62.8298 1.20084 73.6998 2.10084',
+    wonky:
+      'M1.5 6.46911C11.71 4.98911 26.84 -0.220894 35.68 5.64911C48.82 14.3691 64.98 10.4991 79.57 8.37911',
+    veryWonky:
+      'M7.3501 9.33977C10.8901 5.20977 19.1201 4.29977 22.2701 4.42977C28.4701 4.66977 36.5701 10.9998 42.3201 12.2098C44.3401 12.5698 49.0601 13.8898 56.3201 13.8598C63.4901 13.1398 69.5601 8.82977 72.6401 1.50977',
+  };
+
   let animationDelay = 0;
 
   useEffect(() => {
-    if (sectionsRef) {
+    if (sectionsRef && sectionsRef.current) {
       const handleScroll = () => {
-        sectionsRef.current.forEach((section, index) => {
-          if (section !== null && section.getBoundingClientRect().top < 500) {
-            setActive(navLinks[index]);
-          }
-        });
+        if (!scrollByClick) {
+          sectionsRef.current.forEach((section, index) => {
+            if (
+              section.getBoundingClientRect().top <
+              window.innerHeight / 2 - 75
+            ) {
+              setActive(navLinks[index]);
+            }
+          });
+        }
       };
       window.addEventListener('scroll', handleScroll);
       return () => window.removeEventListener('scroll', handleScroll);
     }
   });
+
+  useEffect(() => {
+    if (scrollByClick) {
+      setTimeout(() => setScrollByClick(false), 1000);
+    }
+  }, [scrollByClick]);
 
   return (
     <div className='hidden md:flex'>
@@ -47,7 +70,10 @@ const NavLinks = ({ sectionsRef, activeLink }: NavLinksProps) => {
                 }`}
                 onMouseOver={() => setHovered(navLink)}
                 onMouseOut={() => setHovered('')}
-                onClick={() => setActive(navLink)}
+                onClick={() => {
+                  setActive(navLink);
+                  setScrollByClick(true);
+                }}
               >
                 {navLink}
               </Link>
